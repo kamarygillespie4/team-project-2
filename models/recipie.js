984e5586a2e00c37b9e6674567ff48b6ecc53c1e
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-09-15 09:17:40 -0600

    recipe model rought draft

---
+++ models/recipie.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create a new Sequelize model for books
+class Recipe extends Model {}
+
+Recipe.init(
+    // Define fields/columns on model
+    // An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
+    {
+        title: {
+            type: DataTypes.STRING
+        },
+        ingedients: {
+            type: DataTypes.STRING
+        },
+        instructions: {
+            type: DataTypes.STRING
+        },
+        cooktime: {
+            type: DataTypes.INTEGER
+        }
+    }, {
+        // Link to database connection
+        sequelize,
+        // Set to false to remove `created_at` and `updated_at` fields
+        timestamps: false,
+        underscored: true,
+        modelName: 'recipe'
+    }
+);
+
+module.exports = Recipe;
