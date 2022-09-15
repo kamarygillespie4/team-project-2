7ffb84508a6552e7a727968c67b1e255aebcc6f9
Jordan <jordan.laczkowski23@gmail.com>
2022-09-15 09:53:08 -0600

    added boiler to seeds index.js

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+// TODO: make sure dependecies are needed/in here
+const sequelize = require("../config/connection");
+const Dish = require("../models/Dish");
+const dishData = require("./dish-seeds.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Dish.bulkCreate(dishData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
