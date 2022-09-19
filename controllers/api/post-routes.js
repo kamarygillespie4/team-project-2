2f684bcadb75c73f71eb06eddc16b03d2d80780d
tjlinch <tjlinch@gmail.com>
2022-09-18 19:09:47 -0600

    stuck on posting to database

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 const { Recipe } = require('../../models/');
 const withAuth = require('../../utils/auth');
+// const { post } = require('./userRoutes');
 
 router.post('/', withAuth, async (req, res) => {
-    
-})
+    const body = req.body;
+
+    try {
+        const newRecipe = await Recipe.create({ ...body, userId: req.session.userId });
+        res.json(newRecipe);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
